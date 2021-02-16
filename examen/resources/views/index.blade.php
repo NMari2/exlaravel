31792fb8cd3ed42bc5099f3148c0e1302be6ac54
Mari <marignacia.flores@gamil.com>
2021-02-16 20:22:11 +0100

    Directices blade

---
+++ examen/resources/views/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts/plantilla');
+@section('cuerpo')
+<h1>inicio</h1>
+@endsection
---
+++ examen/resources/views/layouts/footer.blade.php
@@ -0,0 +1,20 @@
+<footer class="bg-dark text-light align-self-end container-fluid p-3 d-flex flex-column align-items-center">
+  <div class="d-flex align-items-center ">
+    <span class="pl-2 fw-bold">SÍGUEME</span>
+  </div>
+  <div class="d-flex justify-content-center" style="font-size:25px;">
+    <a class="text-light p-3" href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+    <a class="text-light p-3" href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+    <a class="text-light p-3" href="https://www.linkedin.com/"><i class="fab fa-linkedin"></i></a>
+    <a class="text-light p-3" href="https://web.telegram.org/#/login"><i class="fab fa-telegram"></i></a>
+  </div>
+  <hr class="featurette-divider w-75">
+  <div class="d-flex flex-column align-items-center fw-lighter">
+    <div class="d-flex ">
+      <a class="text-light p-2" href="#">Aviso Legal</a>
+      <a class="text-light p-2" href="#">Política De Privacidad</a>
+      <a class="text-light p-2" href="#">Política De Cookies</a>
+    </div>
+    <p>© 2021 | Mi Blog </p>
+  </div>
+</footer>
