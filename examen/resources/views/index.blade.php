be20530ba6ffe042b745cbae221752fe9885faab
Mari <marignacia.flores@gamil.com>
2021-02-16 20:36:18 +0100

    pagina inicio

--- examen/resources/views/index.blade.php
+++ examen/resources/views/index.blade.php
@@ -1,4 +1,8 @@
 @extends('layouts/plantilla');
 @section('cuerpo')
-<h1>inicio</h1>
+
+<main class="mt-5">
+  <h1>Bienvenido!</h1>
+  <img src="https://images.pexels.com/photos/6745118/pexels-photo-6745118.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="">
+</main>
 @endsection
