0dc022160327cb3840c0d97cf0d4fcc984cf3d40
Mari <marignacia.flores@gamil.com>
2021-02-16 20:28:58 +0100

    Footer y Navegador

--- examen/resources/views/layouts/footer.blade.php
+++ examen/resources/views/layouts/footer.blade.php
@@ -9,12 +9,10 @@
     <a class="text-light p-3" href="https://web.telegram.org/#/login"><i class="fab fa-telegram"></i></a>
   </div>
   <hr class="featurette-divider w-75">
-  <div class="d-flex flex-column align-items-center fw-lighter">
-    <div class="d-flex ">
-      <a class="text-light p-2" href="#">Aviso Legal</a>
-      <a class="text-light p-2" href="#">Política De Privacidad</a>
-      <a class="text-light p-2" href="#">Política De Cookies</a>
-    </div>
-    <p>© 2021 | Mi Blog </p>
+  <div class="">
+
+  <h2>Marignacia Belén Flores Muñoz<small>2º DAW</small></h2>
+  <p>{{ date("d-m-Y") }}</p>
+    <p>© 2021 | Examen </p>
   </div>
 </footer>
